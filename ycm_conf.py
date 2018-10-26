c1c15b58b683ace61694d9d046b27cb21d4d4421
Matt McCredie <matt.mccredie@vacasa.com>
2018-10-26 11:37:20 -0700

    Updating ycm config to work with local and pipenv python versions

--- ycm_conf.py
+++ ycm_conf.py
@@ -3,5 +3,8 @@ import subprocess
 def Settings( **kwargs ):
     client_data = kwargs[ 'client_data' ]
     return {
-        'interpreter_path': subprocess.check_output(['pipenv', '--py']).strip()
+        'interpreter_path': subprocess.check_output(
+            'pipenv --py || which python',
+            shell=True).strip()
     }
+
